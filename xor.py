5c1ca6ba948e427fd1a638cf3a13e5e22007f56d
nick3rson <nick01.nick02.nick03@gmail.com>
2026-05-19 23:26:49 +0700

    add xor.py

--- xor.py
+++ xor.py
@@ -1,4 +1,4 @@
-plaintext = "save my world!!"
+from itertools import cycle
 
 def string_to_binary(s):
     b = []
@@ -6,4 +6,22 @@ def string_to_binary(s):
         b.append("{:08b}".format(ord(c)))
     return b
 
-print(" ".join(string_to_binary(plaintext)))
+def xor_cipher(plaintext , key):
+    x = []
+    for t,k in zip(plaintext , cycle(key)):
+        x.append(chr(ord(t) ^ ord(k)))
+    return x
+
+p = input("plaintext: ")
+k = input("key: ")
+
+encrypt = xor_cipher(p, k)
+
+print(encrypt)
+print("".join(encrypt))
+
+#decrypt
+decrypt = xor_cipher(encrypt, k)
+print(decrypt)
+print("".join(decrypt))
+#print(" ".join(string_to_binary(plaintext)))
